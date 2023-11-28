a60eb9e6f99975f927b0a240459430c60ebd70c1
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2023-11-28 20:58:57 +0100

    init

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+
+darkModeButton = document.getElementById("dark-mode-button")
+
+darkModeButton.addEventListener('click', function (e) {
+    let main = document.getElementsByTagName('main')[0]
+
+    main.classList.toggle("light-mode");
+
+    console.log(main);
+    
+    
+    
+
+
+
+
+
+})
+
+console.log(darkModeButton);
+
+
